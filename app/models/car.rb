c6296565e09a42884d2a3e2256b4e6a8c68c5c00
Alex Lauderdale <alauderdale@mac.com>
2013-08-08 12:28:43 -0600

    clip images

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,5 +2,5 @@ class Car < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :user
   validates_numericality_of :year, :only_integer => true
-  has_attached_file :car_photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/car_missing.png"
+  has_attached_file :car_photo, :styles => { :medium => "400x300#", :thumb => "220x160#" }, :default_url => "/images/:style/car_missing.png"
 end
