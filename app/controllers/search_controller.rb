42cdff25a601075e1e8caf824a8b4030286ec50c
Alex Lauderdale <alauderdale@mac.com>
2013-08-08 16:50:52 -0600

    search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
 	def index
 		# @search_cars = Car.search (params[:search], car_model: params[:search] ).all
-		@search_cars = Car.search(params[:search]).all
+		@search_cars = Car.search(params[:search])
 		@search_users = User.search(name: params[:search]).all
 	end
 end
